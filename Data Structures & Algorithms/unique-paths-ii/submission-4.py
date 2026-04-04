951671b24fe1ea7df29c059b268eb77469f4db61
ChengTing <156295425+chengting0906os@users.noreply.github.com>
2026-04-04 19:38:28 +0800

    Add: unique-paths-ii - submission-4

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        """
+        Plan: if obstacleGrid[i][j] == 0 dp[i][j] = 0
+
+        Time Complexity: m * n
+        Space Complexity: m * n
+        """
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+
+        dp = [0] * n 
+        dp[0] = 1
+
+        for i in range(m):
+            if obstacleGrid[i][0] == 1:
+                dp[0] = 0 
+            
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                else:
+                    dp[j] += dp[j-1]
+
+        return dp[n-1]
